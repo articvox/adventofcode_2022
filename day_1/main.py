d0cf9ed2eebfdf39f9507183fba01aecf776d4a2
articvox <67555967+articvox@users.noreply.github.com>
2022-12-13 13:55:30 +0200

    Day 1 step 2 solution

--- day_1/main.py
+++ day_1/main.py
@@ -7,5 +7,10 @@ def get_input():
 
 
 if __name__ == '__main__':
+    # step 1
     calories = [list([int(cal) for cal in group]) for key, group in groupby(get_input(), lambda x: x == '') if not key]
-    print(max([sum(group_calories) for group_calories in calories]))
+    total_calories = [sum(group_calories) for group_calories in calories]
+    print(max(total_calories))
+
+    # step 2
+    print(sum(sorted(total_calories, reverse=True)[:3]))
